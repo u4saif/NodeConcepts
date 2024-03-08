8633306accc3f82c4690e27eb72dac9a5d09c19f
eysaif <u4saif@gmail.com>
2024-03-08 16:54:44 +0530

    optimized multi threadding

--- multiThreadOptimized/server.js
+++ multiThreadOptimized/server.js
@@ -1,23 +1,32 @@
 import express from "express";
 import {Worker} from "worker_threads";
+const THREADS = 3;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const createThread = ()=>{
+    return new Promise((resolve,reject)=>{
+        let worker = new Worker("./workerThread.js",{
+            workerData:{"THREAD_COUNT":THREADS}
+        });
+        worker.on("message",(data)=>  resolve(data));
+        worker.on('error',(data)=> reject(data));
+    })
+}
+
 app.get("/nonblocking",(req,res)=>{
-    res.status(200).send("Data from Non blocking");
+    res.status(200).send({"data":`Data from Non blocking:ID ${req.query?.id}`});
 })
 
-app.get("/blocking", (req,res)=>{
-    let worker = new Worker("./workerThread.js");
-    
-    worker.on("message",(data)=>{
-        res.status(200).send({"message":"Data from Blocking","data":data});
-    });
-
-    worker.on("error",(data)=>{
-        res.status(500).send({"message":"Error comming from Blocking","data":data})
-    });
+app.get("/blocking", async (req,res)=>{
+    let workers=[]
+    for(let i = 0; i< THREADS; i++){
+        workers[i] = createThread();
+    }
+    const results = await Promise.all(workers);
+    const finalResult = results[0]+results[1]+results[2];
+    res.status(200).send({"message":`Data from Blocking :ID ${req.query?.id}`,"data":finalResult});
 })
 
 app.listen(PORT,()=>{
--- multiThreadOptimized/workerThread.js
+++ multiThreadOptimized/workerThread.js
@@ -1,8 +1,8 @@
-import { parentPort } from "worker_threads";
+import { workerData,parentPort } from "worker_threads";
 
 const BigDataComputaion = ()=>{
     let result = 0;
-    for(let i=0; i< 30_000_000_00;i++){
+    for(let i=0; i< 30_000_000_00/workerData.THREAD_COUNT;i++){
         result+=1;
     } 
     return result;
