8efa2b06a844a051760c47f31524149a6dc8e54f
eysaif <u4saif@gmail.com>
2024-03-08 21:10:31 +0530

    custor main js added

--- clusterSample/index.js
+++ clusterSample/index.js
@@ -2,11 +2,21 @@ import express from "express";
 
 const PORT = process.env.port || 3000;
 const app = express();
-
-app.get("/inode",(req,res)=>{
-    console.log(req.query);
-    res.status(200).send({"message":"Hello from NODE cluster"});
+const computeData = () => {
+  let count = 0;
+  for (let i = 0; i < 20_000_000; i++) {
+    count += 1;
+  }
+  return count;
+};
+app.get("/compute", (req, res) => {
+  // console.log(req.query);
+  let largeNum = computeData();
+  res.status(200).send({ message: "Hello from NODE cluster", data: largeNum });
 });
-app.listen(PORT,()=>{
-    console.log("Server started as PORT🚀🚀🚀",PORT);
-})
+
+app.listen(PORT, () => {
+  console.log(
+    `Server started at PORT ${PORT} 🚀🚀🚀 with Process ID ${process.pid}🚀`
+  );
+});
