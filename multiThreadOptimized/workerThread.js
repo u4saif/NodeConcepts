ce654309cb1d701561a2690e18f247727af43863
shaikh Saifuddin <u4saif@gmail.com>
2024-03-11 13:20:45 +0530

    Update workerThread.js

--- multiThreadOptimized/workerThread.js
+++ multiThreadOptimized/workerThread.js
@@ -1,11 +1,11 @@
-import { workerData,parentPort } from "worker_threads";
+import { workerData, parentPort } from "worker_threads";
 
-const BigDataComputaion = ()=>{
-    let result = 0;
-    for(let i=0; i< 30_000_000_00/workerData.THREAD_COUNT;i++){
-        result+=1;
-    } 
-    return result;
-}
- 
+const BigDataComputaion = () => {
+  let result = 0;
+  for (let i = 0; i < 30_000_000_00 / workerData.THREAD_COUNT; i++) {
+    result += 1;
+  }
+  return result;
+};
+
 parentPort.postMessage(BigDataComputaion());
