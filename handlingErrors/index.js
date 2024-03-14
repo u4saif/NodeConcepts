8eacbd720613384021177424ac2f6e886c965f36
shaikh Saifuddin <u4saif@gmail.com>
2024-03-14 16:16:58 +0530

    using Joi for more errors
    Co-Authored-By: Shaikh Saifuddin <119283516+eysaif@users.noreply.github.com>

--- handlingErrors/index.js
+++ handlingErrors/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
 import tryCatch from "./utils/tryCatch.js";
+import Joi from "joi";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const userDetails = () => undefined;
+const schema = Joi.object({
+  userId:Joi.number().required()
+})
 
 app.get(
   "/login",
@@ -12,6 +16,20 @@ app.get(
     const user = await userDetails();
     if (!user) {
       throw new Error("User not found");
+    }
+    res.status(200).send({ message: `Wellcome back 🤘 ${user}` });
+
+    next(error);
+  })
+);
+
+app.post(
+  "/login",
+  tryCatch(async (req, res, next) => {
+    const user = await userDetails();
+    const { error, value} = schema.validate({});
+    if(error){
+      throw error;
     }
     res.status(200).send({ message: `Wellcome back 🤘 ${user}` });
 
--- handlingErrors/middleware/errorHandler.js
+++ handlingErrors/middleware/errorHandler.js
@@ -1,4 +1,9 @@
 const errorHandler = (error, req, res, next) => {
+  if (error.name === "ValidationError") {
+      res.status(400).send({ 
+        type: "ValidationError",
+        details: error.details });
+  }
   res.status(400).send({ message: error.message });
 };
 
