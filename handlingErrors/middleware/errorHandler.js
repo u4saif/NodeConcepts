ddf2a93f0ee5a963a5331b7a3f02f696397049fb
shaikh Saifuddin <u4saif@gmail.com>
2024-03-14 16:18:55 +0530

    using Joi for more errors
    Co-Authored-By: Shaikh Saifuddin <119283516+eysaif@users.noreply.github.com>

--- handlingErrors/middleware/errorHandler.js
+++ handlingErrors/middleware/errorHandler.js
@@ -1,10 +1,11 @@
 const errorHandler = (error, req, res, next) => {
   if (error.name === "ValidationError") {
-      res.status(400).send({ 
-        type: "ValidationError",
-        details: error.details });
+    return res.status(400).send({
+      type: "ValidationError",
+      details: error.details,
+    });
   }
-  res.status(400).send({ message: error.message });
+  return res.status(400).send({ message: error.message });
 };
 
 export default errorHandler;
