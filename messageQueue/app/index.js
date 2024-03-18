f6a24f9290930b025ccb4fd9708a945c6fec7f01
shaikh Saifuddin <u4saif@gmail.com>
2024-03-18 23:07:30 +0530

    Message QUEUE
    Message queue implemented in Nodejs with BULL and Redis
    
    Co-Authored-By: Shaikh Saifuddin <119283516+eysaif@users.noreply.github.com>

--- messageQueue/app/index.js
+++ messageQueue/app/index.js
@@ -1,20 +1,63 @@
 import express from "express";
-const PORT = process.env.PORT || 3000;
+import Bull from "bull";
+import dotenv from "dotenv";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/queue",(req,res)=>{
-    res.status(200).send({
-        message:"Please load your Data for processing."
-    })
+//TO READ THE REQUEST BODY
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+dotenv.config();
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
+const redisOptions = {
+  redis: { host: REDIS_HOST, port: REDIS_PORT, password: REDIS_PASSWORD },
+};
+
+// DEFINE QUEUE
+const burgerQueue = new Bull("burger", redisOptions);
+
+//Stor Processed Data
+const processedItems = [];
+
+app.get("/queue", async (req, res) => {
+  res.status(200).send({
+    message: "Please load your Data for processing.",
+    data: {
+      processedItems: processedItems,
+      message: "Items processed Till now.",
+    },
+  });
 });
 
-app.put("/process",(req,res)=>{
-    res.status(200).send({
-        message:"Please load your Data for processing."
-    })
-})
+app.post("/process", (req, res) => {
+  //ADD JOB TO THE QUEUE
+  const { inventory } = req.body;
+  console.log(inventory);
+  burgerQueue.add(inventory);
+  res.status(200).send({
+    message: "Please wait loading your inventory.",
+  });
+});
 
-app.listen(PORT,()=>{
-    console.log("App is running at PORT: 🚀",PORT)
-})
+// REGISTER PROCESSER
+burgerQueue.process((payload, done) => {
+  console.log("Preparing the burger!");
+  setTimeout(() => {
+    console.log("Burger ready!",payload.data);
+    processedItems.push(payload.data);
+    done();
+  }, 10000);
+});
+
+app.listen(PORT, () => {
+  console.log("App is running at PORT: 🚀", PORT);
+});
+
+// //ADD JOB TO THE QUEUE
+// burgerQueue.add({
+//   bun: "🍔",
+//   cheese: "🧀",
+//   toppings: ["🍅", "🫒", "🥒", "🌶️"],
+// });
---
+++ messageQueue/docker-setup.sh
@@ -0,0 +1,5 @@
+echo "Building the Application"
+docker-compose build
+
+echo "Running the Container"
+docker-compose up
